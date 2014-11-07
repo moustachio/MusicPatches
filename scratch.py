a99dcb8b1c4278539d16cc37727cbab5ce7b8924
Jared Lorince <rexgoliath@gmail.com>
2014-11-07 13:42:11 -0500

    minor updates

--- scratch.py
+++ scratch.py
@@ -1,13 +1,13 @@
 from scipy.spatial.distance import cosine
 from pyechonest import config
 from pyechonest.artist import Artist
-from pyechonest.util import EchoNestAPIError
+from pyechonest.util import EchoNestAPIError,EchoNestIOError
 from urllib2 import unquote
 import socket
 import time
 import MySQLdb
 import itertools
-import multiprocessing as mp
+#import multiprocessing as mp
 import math
 
 
@@ -21,17 +21,20 @@ def echonest_terms(artist):
 	while True:
 		try:
 			return {i['name']:i['weight'] for i in Artist(artist).terms}
+		except EchoNestIOError:
+			time.sleep(5)
 		except EchoNestAPIError,error:
 			http_status = error.http_status
 			# If the artist is not found, return empty dict
 			# EchoNestAPIError: (u'Echo Nest API Error 5: The Identifier specified does not exist [HTTP 200]',)
-			if http_status == 200:
+			if http_status in [200,400]:
 				return {}
 			# If we've exceeded the rate limit, wait 5 seconds and then try again
 			# EchoNestAPIError: (u'Echo Nest API Error 3: 3|You are limited to 120 accesses every minute. You might be eligible for a rate limit increase, go to http://developer.echonest.com/account/upgrade [HTTP 429]',)
 			elif http_status == 429:
 				time.sleep(5)
 			# Any other error, raise the exception
+
 			else:
 				raise error
 		except socket.error:
@@ -87,7 +90,76 @@ def get_sim(artist1_id,artist1_name,artist2_id,artist2_name):
 		db.commit()
 		return sim
 
+#Processes a listening time series retrieved from database, represented as a tuple:
+#	((artist_id_1,artist_name_1),(artist_id_2,artist_name_2),...,(artist_id_b,artist_name_n))
+#Returns list of artist similarities of length n-1, where n is the length of the time series.
 
+def process_artist_seq(seq,verbose=False):
+	#start = time.time()
+	sims = []
+	last_id = seq[0][0]
+	last_name = seq[0][1].replace('+',' ')
+	#for i,(artist_id,artist_name) in enumerate(seq[1:]):
+	for artist_id,artist_name,ts in seq[1:]:
+		artist_name = unquote(artist_name).replace('+',' ')
+		if artist_id == last_id:
+			sim = 1.0
+		else:
+			# THIS TRY/EXCEPT BLOCK IS FOR DEBUGGING ONLY
+			#try:
+			sim = get_sim(artist_id,artist_name,last_id,last_name)
+			#except: 
+			#	print artist_id,artist_name,last_id,last_name
+			#	return artist_id,artist_name,last_id,last_name
+		sims.append(sim)
+		if verbose:
+			print artist_id,artist_name,last_id,last_name,sim
+		last_id = artist_id
+		last_name = artist_name
+	return sims
+
+#Simple, temporary function to handle writing output to file
+def output_handler(user,listening_seq,sim_seq,fi,filemode):
+	out = open(fi,filemode)
+	for (artist_id,artist_name,ts),sim in zip(listening_seq,[None]+sim_seq):
+		out.write('\t'.join(map(str,[user,artist_id,artist_name,ts,sim]))+'\n')
+	out.close()
+
+
+config.ECHO_NEST_API_KEY=open('api.key').read()
+mysql_user,mysql_pass = [line.strip() for line in open('mysql.key')]
+
+out = open('sim_seqs.tsv','w')
+
+db = MySQLdb.connect(host="127.0.0.1", user=mysql_user, passwd=mysql_pass,db="analysis_lastfm")
+cursor = db.cursor()
+
+n = cursor.execute("select user_id from lastfm_users where sample_playcount>=100;")
+users = cursor.fetchall()
+
+
+fi = 'E:/BTSync/Research.Archive/LastFM/MusicPatches/simSeqs.tsv'
+out = open(fi,'w')
+for i,user in enumerate(users):
+	scrobbles = cursor.execute("select s.artist_id,i.artist,s.scrobble_time from lastfm_scrobbles s join lastfm_itemlist i on s.artist_id=i.item_id where \
+				user_id = %s order by scrobble_time asc", (user[0],))
+	#scrobbles = cursor.execute("select artist_id from lastfm_scrobbles where user_id=%s order by scrobble_time asc", (user[0],))
+	result = cursor.fetchall()
+	sims = process_artist_seq(result)
+	out.write('\t'.join(map(str,[user]+sims))+'\n')
+	print 'user %s (%s scrobbles)' % (user[0],scrobbles), '%s / %s' % (i+1,n), '%0.2f percent complete' % (100*((i+1.)/n))
+	#if i==0:
+	#	output_handler(user[0],result,sims,fi,'w')
+	#else:
+	#	output_handler(user[0],result,sims,fi,'a')
+out.close()
+
+cursor.close()
+db.close()
+
+
+
+"""
 if __name__ == '__main__':
 
 	config.ECHO_NEST_API_KEY=open('api.key').read()
@@ -125,48 +197,10 @@ if __name__ == '__main__':
 		out.write('\n')
 	cursor.close()
 	db.close()
-
-
-
-###### EVERYTHING BELOW HERE IS JUNK
 """
 
-#Processes a listening time series retrieved from database, represented as a tuple:
-#	((artist_id_1,artist_name_1),(artist_id_2,artist_name_2),...,(artist_id_b,artist_name_n))
-#Returns list of artist similarities of length n-1, where n is the length of the time series.
 
-def process_artist_seq(seq,verbose=False):
-	#start = time.time()
-	sims = []
-	last_id = seq[0][0]
-	last_name = seq[0][1].replace('+',' ')
-	#for i,(artist_id,artist_name) in enumerate(seq[1:]):
-	for artist_id,artist_name,ts in seq[1:]:
-		artist_name = unquote(artist_name).replace('+',' ')
-		if artist_id == last_id:
-			sim = 1.0
-		else:
-			# THIS TRY/EXCEPT BLOCK IS FOR DEBUGGING ONLY
-			#try:
-			sim = get_sim(artist_id,artist_name,last_id,last_name)
-			#except: 
-			#	print artist_id,artist_name,last_id,last_name
-			#	return artist_id,artist_name,last_id,last_name
-		sims.append(sim)
-		if verbose:
-			print artist_id,artist_name,last_id,last_name,sim
-		last_id = artist_id
-		last_name = artist_name
-	return sims
 
-#Simple, temporary function to handle writing output to file
-
-def output_handler(user,listening_seq,sim_seq,fi,filemode):
-	out = open(fi,filemode)
-	for (artist_id,artist_name,ts),sim in zip(listening_seq,[None]+sim_seq):
-		out.write('\t'.join(map(str,[user,artist_id,artist_name,ts,sim]))+'\n')
-	out.close()
-"""
 
 		
 	
